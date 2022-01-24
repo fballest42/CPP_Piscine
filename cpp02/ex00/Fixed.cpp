4a0b652ed256ac35353e0cd331d4077ce99e4e98
Fernando Ballesteros santamaria <fballest@c1r7s5.42madrid.com>
2022-01-24 16:02:29 +0100

    Begining cpp02 ex00

--- cpp02/ex00/Fixed.cpp
+++ cpp02/ex00/Fixed.cpp
@@ -6,13 +6,13 @@
 /*   By: fballest <fballest@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/24 14:58:59 by fballest          #+#    #+#             */
-/*   Updated: 2022/01/24 15:07:31 by fballest         ###   ########.fr       */
+/*   Updated: 2022/01/24 16:02:06 by fballest         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 
-Fixed(void)
+Fixed(void): _snum(0);
 {
 	std::cout << "Fixed has been contructed." << std::endl;
 }
@@ -30,4 +30,14 @@ Fixed& operator=(Fixed const &)
 ~Fixed(void)
 {
 	std::cout << "Fixed has been destroyed." << std::endl;
+}
+
+int Fixed::getRawBits(void) const
+{
+	return (this->_snum);
+}
+
+void Fixed::setRawBits (int const raw);
+{
+	this->_snum = raw;
 }
