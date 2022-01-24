c5d9876ecd6ec3f0295b11ab7008e13d770d8a0c
Fernando Ballesteros santamaria <fballest@c1r7s5.42madrid.com>
2022-01-24 14:28:13 +0100

    cpp01 ex06 just finished but with some dudes 24-1-2022 14:30

--- cpp01/ex06/Karen.cpp
+++ cpp01/ex06/Karen.cpp
@@ -6,7 +6,7 @@
 /*   By: fballest <fballest@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/24 10:59:43 by fballest          #+#    #+#             */
-/*   Updated: 2022/01/24 13:28:28 by fballest         ###   ########.fr       */
+/*   Updated: 2022/01/24 14:26:42 by fballest         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,22 +26,23 @@ void	Karen::debug( void )
 {
 
 	std::cout << "I love to get extra bacon for my "
-			<< "7XL-double-cheese-triple-pickle-special-ketchup burger. I just love it!."
+			<< "7XL-double-cheese-triple-pickle-special-ketchup burger." << std::endl
+			<< "I just love it!."
 			<< std::endl;
 }
 
 void	Karen::info( void )
 {
 
-	std::cout << "I cannot believe adding extra bacon cost more money. " 
+	std::cout << "I cannot believe adding extra bacon cost more money. " << std::endl
 			<< "You don’t put enough! If you did I would not have to ask for it!."
 			<< std::endl;
 }
 
 void	Karen::warning( void )
 {
-	std::cout << "I think I deserve to have some extra bacon for free."
-			<< " I’ve been coming here for years and "
+	std::cout << "I think I deserve to have some extra bacon for free." << std::endl
+			<< "I’ve been coming here for years and "
 			<< "you just started working here last month." << std::endl;
 }
 
@@ -57,20 +58,53 @@ void	Karen::exit(void)
 
 void	Karen::complain(std::string msg)
 {
-	std::string	msgs[5] = {"DEBUG", "INFO", "WARNING", "ERROR", "EXIT"};
+	std::string	msgs[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
 		
 	funtion[0] = &Karen::debug;
 	funtion[1] = &Karen::info;
 	funtion[2] = &Karen::warning;
 	funtion[3] = &Karen::error;
 
-	int		i = 0;
-	while (i >= 0 && i < 5)
-	{
-		if (i == 4)
-			this->exit();
-		if (msgs[i] == msg)
-			(this->*funtion[i])();
+	int i = 0;
+	while (msgs[i] != msg && i < 4)
 		i++;
+	while (i <= 4)
+	{
+		switch (i)
+		{
+			case 0:
+				std::cout << "[ " << msgs[0] << " ]" << std::endl;
+				(this->*funtion[i++])();
+				break;
+			case 1:
+				std::cout << "[ " << msgs[1] << " ]" << std::endl;
+				(this->*funtion[i++])();
+				break;
+			case 2:
+				std::cout << "[ " << msgs[2] << " ]" << std::endl;
+				(this->*funtion[i++])();
+				break;
+			case 3:
+				std::cout << "[ " << msgs[3] << " ]" << std::endl;
+				(this->*funtion[i++])();
+				break;
+			default:
+				std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+				i++;
+				break;	
+		}
 	}
+	// int		i = 0;
+	// while (i >= 0 && i < 4)
+	// {
+	// 	if (msgs[i] == msg)
+	// 		while (i < 4)
+	// 		{
+	// 			std::cout << "[ " << msgs[i] << " ]" << std::endl;
+	// 			(this->*funtion[i++])();
+	// 		}
+	// 	i++;
+	// }
+	// if (i < 0 || i >= 4)
+	// 	std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
 }
