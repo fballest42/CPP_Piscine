eada27d17db68d59e63e5205900fa46d7c6df397
Fernando Ballesteros santamaria <fballest@c1r7s5.42madrid.com>
2022-01-24 14:28:56 +0100

    some small modifications

--- cpp01/ex05/Karen.cpp
+++ cpp01/ex05/Karen.cpp
@@ -6,7 +6,7 @@
 /*   By: fballest <fballest@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/24 10:59:43 by fballest          #+#    #+#             */
-/*   Updated: 2022/01/24 13:14:41 by fballest         ###   ########.fr       */
+/*   Updated: 2022/01/24 13:42:35 by fballest         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,28 +26,24 @@ void	Karen::debug( void )
 {
 
 	std::cout << "I love to get extra bacon for my "
-			<< "7XL-double-cheese-triple-pickle-special-ketchup burger. I just love it!."
+			<< "7XL-double-cheese-triple-pickle-special-ketchup burger." << std::endl
+			<< "I just love it!."
 			<< std::endl;
 }
 
 void	Karen::info( void )
 {
 
-	std::cout << "I cannot believe adding extra bacon cost more money. " 
+	std::cout << "I cannot believe adding extra bacon cost more money. " << std::endl
 			<< "You don’t put enough! If you did I would not have to ask for it!."
 			<< std::endl;
 }
 
 void	Karen::warning( void )
 {
-	std::cout << "I think I deserve to have some extra bacon for free."
-			<< " I’ve been coming here for years and "
+	std::cout << "I think I deserve to have some extra bacon for free." << std::endl
+			<< "I’ve been coming here for years and "
 			<< "you just started working here last month." << std::endl;
-}
-
-void	Karen::error( void )
-{
-	std::cout << "This is unacceptable, I want to speak to the manager now." << std::endl;
 }
 
 void	Karen::complain(std::string msg)
