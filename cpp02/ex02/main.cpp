243c14dcebd1ce6d5a6f6a89f0349d9c94215acf
Fernando Ballesteros santamaria <fballest@c1r13s5.42madrid.com>
2022-01-28 10:55:52 +0100

    CPP02 EX02 finished and working properly

--- cpp02/ex02/main.cpp
+++ cpp02/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: fballest <fballest@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/24 15:39:06 by fballest          #+#    #+#             */
-/*   Updated: 2022/01/28 10:26:16 by fballest         ###   ########.fr       */
+/*   Updated: 2022/01/28 10:54:39 by fballest         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,6 @@ int main( void )
 {
 	Fixed a;
 	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
-	Fixed const c( Fixed( 5.05f ) / Fixed( 2 ) );
-	Fixed const d( Fixed( 5.05f ) + Fixed( 2 ) );
-	Fixed const e( Fixed( 5.05f ) - Fixed( 2 ) );
 	
 	std::cout << a << std::endl;
 	std::cout << ++a << std::endl;
@@ -28,8 +25,6 @@ int main( void )
 	std::cout << a << std::endl;
 	std::cout << b << std::endl;
 	std::cout << Fixed::max( a, b ) << std::endl;
-	std::cout << Fixed::min( a, b ) << std::endl;
-	std::cout << c << std::endl;
 	
 	return 0;
 }
